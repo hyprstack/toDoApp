44cf76505167cb0cd92cf0addc0ad6bbafff09d2
MMendes999 <mario.c.mendes1985@gmail.com>
2014-11-14 14:14:29 +0000

    adds css, backbone localStorage

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// AppView and todoView need to be instantiated
+
+  var app = app || {};
+  var ENTER_KEY = 13;
+
+  $(function() {
+
+    // Kick things off by creating the **App**.
+    new app.AppView();
+
+  });
